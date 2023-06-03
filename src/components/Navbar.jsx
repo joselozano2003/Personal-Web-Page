44800b6cb78dd45ea62fb3a32b42faa83420146e
JoseLozano2003 <josecamilo.lozanocet@ucalgary.ca>
2023-06-03 00:08:05 -0600

    Changed UI a bit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,109 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../assets/logo-no-background.svg';
 import { Link } from 'react-scroll';
 
 function Navbar() {
+  const [activeLink, setActiveLink] = useState('');
 
-    const styles = {
-        link: {
-            cursor: 'pointer',
-        },
+  const handleSetActive = (to) => {
+    setActiveLink(to);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const sections = ['hero-section', 'about-me', 'tech-section', 'portfolio-section'];
+      const currentSection = sections.find((section) => {
+        const element = document.getElementById(section);
+        if (element) {
+          const offsetTop = element.offsetTop - 60; // Adjust offset as needed
+          const offsetBottom = offsetTop + element.offsetHeight;
+          return scrollPosition >= offsetTop && scrollPosition < offsetBottom;
+        }
+        return false;
+      });
+
+      setActiveLink(currentSection || '');
     };
-  
-    return (
-        <nav className="navbar">
-            <div className="navbar__logo">
-                <Link to='hero-section' spy={true} smooth={true} offset={0} duration={500} style={styles.link}>
-                    <img src={Logo} className="logo" alt="React logo" />
-                </Link>
-            </div>
-            <div className="navbar__links">
-                <ul className="navbar-ul">
-                    <Link to='about-me' spy={true} smooth={true} offset={-60} duration={500} style={styles.link} className="navbar__link">
-                        <li className="navbar-item">About</li>
-                    </Link>
-                    <Link to='tech-section' spy={true} smooth={true} offset={-60} duration={500} style={styles.link} className="navbar__link">
-                        <li className="navbar-item">Skills</li>
-                    </Link>
 
-                    <a href="#portfolio-section" className="navbar__link">
-                        <li className="navbar-item">Portfolio</li>
-                    </a>
-                </ul>
-            </div>
-        </nav>
-    );
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const styles = {
+    link: {
+      cursor: 'pointer',
+      color: 'white',
+    },
+    activeLink: {
+      color: 'orange',
+    },
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <Link
+          to="hero-section"
+          spy={true}
+          smooth={true}
+          offset={0}
+          duration={500}
+          style={activeLink === '' ? { ...styles.link, ...styles.activeLink } : styles.link}
+          onSetActive={handleSetActive}
+        >
+          <img src={Logo} className="logo" alt="React logo" />
+        </Link>
+      </div>
+      <div className="navbar__links">
+        <ul className="navbar-ul">
+          <Link
+            to="about-me"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={500}
+            style={
+              activeLink === 'about-me'
+                ? { ...styles.link, ...styles.activeLink }
+                : styles.link
+            }
+            onSetActive={handleSetActive}
+            className="navbar__link"
+          >
+            <li className="navbar-item">About</li>
+          </Link>
+          <Link
+            to="tech-section"
+            spy={true}
+            smooth={true}
+            offset={-60}
+            duration={500}
+            style={
+              activeLink === 'tech-section'
+                ? { ...styles.link, ...styles.activeLink }
+                : styles.link
+            }
+            onSetActive={handleSetActive}
+            className="navbar__link"
+          >
+            <li className="navbar-item">Skills</li>
+          </Link>
+          <a
+            href="#portfolio-section"
+            className="navbar__link"
+            style={
+              activeLink === 'portfolio-section'
+                ? { ...styles.link, ...styles.activeLink }
+                : styles.link
+            }
+          >
+            <li className="navbar-item">Portfolio</li>
+          </a>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 import { BallCanvas } from "./canvas";
 import { technologies, languages } from "../constants";
 
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Technologies() {
     return (
         <section id='tech-section'>
             <div className={styles.parent}>
                 <h1 className={styles.gradient}>Skills</h1>
-                <h2>Languages</h2>
+                <h2 className={styles.moving}>Languages</h2>
                 <div className={styles.container}>
                     {languages.map((tech) => (
                         <div className={styles.ball} key={tech.name}>
@@ -18,7 +19,19 @@ export default function Technologies() {
                         </div>
                     ))}
                 </div>
-                <h2>Frameworks</h2>
+                <TypeAnimation 
+                sequence={[
+                    'Frameworks',
+                    1500,
+                    'Libraries',
+                    1500,
+                    'Tools',
+                    1500,
+                  ]}
+                speed={50}
+                className={styles.moving}
+                repeat={Infinity}
+                />
                 <div className={styles.container}>
                     {technologies.map((tech) => (
                         <div className={styles.ball} key={tech.name}>
